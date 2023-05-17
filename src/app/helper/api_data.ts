0ccd4affe533d5372ba809f6aaac5c14b44aca4c
themchoise <maximilianojoyce@gmail.com>
2023-05-17 12:47:59 -0300

    fix api url

--- src/app/helper/api_data.ts
+++ src/app/helper/api_data.ts
@@ -1,3 +1,3 @@
- export const apiData:string = 'http://localhost:8080/'
+ //export const apiData:string = 'http://localhost:8080/'
 //export const apiData:string = 'http://191.101.235.229:8080/'
-//export const apiData:string = 'https://api.maximilianojoyce.com.ar:8081/'
+export const apiData:string = 'https://api.maximilianojoyce.com.ar:8081/'
